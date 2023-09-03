fec4d089c3e5f8c3cb0e4d8a3c8712b9fd9645b1
Eligio Biasi <ex_98@live.it>
2023-09-03 21:04:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+  import AppMain from './components/AppMain.vue';
+  import AppHeader from './components/AppHeader.vue';
+  import AppFooter from './components/AppFooter.vue';
+
+  export default{
+    name: 'App',
+    components:{
+      AppMain,  
+      AppHeader,  
+      AppFooter,  
+    }
+  }
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+    <AppMain />
+  </main>
+  <footer>
+    <AppFooter />
+  </footer>
+</template>
+
+<style lang="scss">
+@use './styles/partials/general.scss' as *;
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>
+            Ciaooo
+        </h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name:'AppMain',
+
+    data() {
+        return {
+            projects:[],
+            apiUrl:'http://127.0.0.1:8000/api/projects',
+        }
+    },
+
+    methods: {
+        getProjects(){
+            axios.get(this.apiUrl, {
+                params: {}
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        },
+    },
+
+    created() {
+        this.getProjects();
+    },
+}
+</script>
+
+<style lang="scss">
+    
+</style>
