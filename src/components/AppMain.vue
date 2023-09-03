927cb2aaa1ea6cf1d04e50a1eecfd4de9e0840f2
Eligio Biasi <ex_98@live.it>
2023-09-03 21:32:44 +0200

    display api throught axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,15 @@
 <template>
-    <div>
+    <div class="main-center">
         <h1>
             Ciaooo
         </h1>
+        <div class="project">
+            <div class="single-project" v-for="project in projects">
+                <h2 >
+                    {{ project.title }}
+                </h2>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,8 +31,9 @@ export default {
             axios.get(this.apiUrl, {
                 params: {}
             })
-            .then(function (response) {
-                console.log(response);
+            .then((response)=>{
+                console.log(response.data.results.data);
+                this.projects= response.data.results.data
             })
             .catch(function (error) {
                 console.log(error);
@@ -40,5 +48,21 @@ export default {
 </script>
 
 <style lang="scss">
-    
+
+    div.main-center{
+        text-align: center;
+
+        div.project{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            align-items: center;
+
+            div.single-project{
+                width: calc((100%/3) - 2rem);
+                border: 2px solid red;
+                margin: 1rem;
+            }
+        }
+    }
 </style>
